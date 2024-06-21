adb9b6a56bd9a31cbc7dd77a8990597f30b680f2
FuadMannan <fuad.mannan@gmail.com>
2024-06-21 00:23:20 -0400

    Add code documentation

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const path = require('path');
 const WALLET_DIR = '.\\wallets';
 
 const CONN = new solWeb3.Connection(
-  solWeb3.clusterApiUrl('devnet'), 'confirmed'
+  solWeb3.clusterApiUrl('devnet'),
+  'confirmed'
 );
 
+/**
+ * Creates new Keypair and saves to file system
+ * @returns {solWeb3.Keypair} Keypair
+ */
 function saveNewFSKeyPair() {
   const keypair = solWeb3.Keypair.generate();
   let filename = keypair.publicKey.toString();
@@ -21,6 +26,12 @@ function saveNewFSKeyPair() {
   return keypair;
 }
 
+/**
+ * Loads a keypair from a file
+ * @async
+ * @param {string} filename Name of file with keypair
+ * @returns {solWeb3.Keypair} Keypair
+ */
 async function getKeyPairFromFile(filename) {
   const keypairPath = path.resolve(WALLET_DIR + '\\' + filename);
   const keypairData = JSON.parse(await fsp.readFile(keypairPath, 'utf-8'));
@@ -28,6 +39,12 @@ async function getKeyPairFromFile(filename) {
   return keypair;
 }
 
+/**
+ * Load keypairs from .json files in a directory
+ * @async
+ * @param {string} directory Location of .json files with keypairs to load. Default is .\wallets\
+ * @returns {Array<solWeb3.Keypair>} Array of keypairs
+ */
 async function getFSWallets(directory = WALLET_DIR) {
   const files = await fsp.readdir(directory);
   const jsonFiles = files.filter(
@@ -40,14 +57,30 @@ async function getFSWallets(directory = WALLET_DIR) {
   return wallets;
 }
 
+/**
+ * Convert lamports to SOL
+ * @param {number} lamports Amount of lamports to convert
+ * @returns {number} Amount of SOL
+ */
 function convertLamportsToSol(lamports) {
   return lamports / solWeb3.LAMPORTS_PER_SOL;
 }
 
+/**
+ * Convert SOL to lamports
+ * @param {number} sol Amount of SOL to convert
+ * @returns {number} Amount of lamports
+ */
 function convertSolToLamports(sol) {
   return sol * solWeb3.LAMPORTS_PER_SOL;
 }
 
+/**
+ * Gets SOL balance for wallet keypairs
+ * @async
+ * @param {Array<solWeb3.Keypair>} walletKeyPairs Array of keypairs
+ * @returns {Array<object>} Array of objects containing a keypair and its balance
+ */
 async function getBalances(walletKeyPairs) {
   const balancePromises = walletKeyPairs.map(async (wallet) => {
     const balance = convertLamportsToSol(
@@ -60,36 +93,73 @@ async function getBalances(walletKeyPairs) {
   return balances;
 }
 
+/**
+ * Creates Transaction object to transfer SOL from one account to another
+ * @param {solWeb3.Keypair} fromKeypair Address transaction is sending from
+ * @param {solWeb3.Keypair} toKeypair Address receiving transaction
+ * @param {number} sol Amount of SOL being transferred
+ * @returns {solWeb3.Transaction} Transaction object
+ */
 function createTXN(fromKeypair, toKeypair, sol) {
   let txn = new solWeb3.Transaction();
   return createTransferInstruction(txn, fromKeypair, toKeypair, sol);
 }
 
+/**
+ * Helper function to create transfer instruction for a transaction
+ * @param {solWeb3.Transaction} txn Transaction to add transfer instruction to
+ * @param {solWeb3.Keypair} fromKeypair Address transaction is sending from
+ * @param {solWeb3.Keypair} toKeypair Address receiving transaction
+ * @param {number} sol Amount of SOL being transferred
+ * @returns {solWeb3.Transaction} Transaction object
+ */
 function createTransferInstruction(txn, fromKeypair, toKeypair, sol) {
   txn.add(
     solWeb3.SystemProgram.transfer({
       fromPubkey: fromKeypair.publicKey,
       toPubkey: toKeypair.publicKey,
-      lamports: convertSolToLamports(sol)
+      lamports: convertSolToLamports(sol),
     })
   );
   return txn;
 }
 
+/**
+ * Helper function to replace transfer instruction for a transaction
+ * @param {solWeb3.Transaction} txn Transaction to add transfer instruction to
+ * @param {solWeb3.Keypair} fromKeypair Address transaction is sending from
+ * @param {solWeb3.Keypair} toKeypair Address receiving transaction
+ * @param {number} sol Amount of SOL being transferred
+ * @returns {solWeb3.Transaction} Transaction object
+ */
 function replaceTransferInstruction(txn, fromKeypair, toKeypair, sol) {
   txn.instructions.pop();
   txn.createTransferInstruction(txn, fromKeypair, toKeypair, sol);
   return txn;
 }
 
+/**
+ * Creates, sends, and confirms a SOL transfer transaction
+ * @param {solWeb3.Keypair} fromKeypair Address transaction is sending from
+ * @param {solWeb3.Keypair} toKeypair Address receiving transaction
+ * @param {number} sol Amount of SOL being transferred
+ * @returns
+ */
 async function transferSol(fromKeypair, toKeypair, sol) {
   const transferTransaction = createTXN(fromKeypair, toKeypair, sol);
   const result = await solWeb3.sendAndConfirmTransaction(
-    CONN, transferTransaction, [fromKeypair]
+    CONN,
+    transferTransaction,
+    [fromKeypair]
   );
   return result;
 }
 
+/**
+ * Calculates estimated transaction fee for a Transaction object
+ * @param {solWeb3.Transaction} transaction
+ * @returns {number} Estimated fee for transaction, returns -1 if null
+ */
 async function calculateTXFee(transaction) {
   const blockhash = await CONN.getLatestBlockhash();
   if (transaction.feePayer == null) {
@@ -108,8 +178,7 @@ async function calculateTXFee(transaction) {
 
 async function main() {
   const walletKeyPairs = await getFSWallets();
-  const balances = await getBalances(walletKeyPairs, CONN)
-  balances.sort((a, b) => {
+  const balances = (await getBalances(walletKeyPairs, CONN)).sort((a, b) => {
     return b.balance - a.balance;
   });
   console.log(balances);
